cd071a02a2352269ee9d017aa14a7285af5d451d
dreamvrutik <vrutikhalani6@gmail.com>
2020-01-22 16:30:33 +0530

    aLL

--- website/school/models.py
+++ website/school/models.py
@@ -1,6 +1,23 @@
 from django.db import models
-
+import os
 # Create your models here.
+def rename(instance,filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (instance.name.replace(' ','_'),ext)
+    return os.path.join('images/', filename)
+
+def rename_event(instance,filename):
+    ext = filename.split('.')[-1]
+    filename = "event_%s.%s" % (instance.name.replace(' ','_'),ext)
+    return os.path.join('images/', filename)
+
 class approach(models.Model):
     name=models.CharField(max_length=264)
     details=models.CharField(max_length=100000)
+    image=models.ImageField(upload_to=rename)
+
+class events(models.Model):
+    name=models.CharField(max_length=264)
+    details=models.CharField(max_length=100000)
+    date=models.DateField()
+    image=models.ImageField(upload_to=rename_event)
--- website/school/views.py
+++ website/school/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from school.models import *
+import shutil
+import datetime
 # Create your views here.
 def index(request):
     obj=approach.objects.all()
@@ -12,8 +14,27 @@ def index(request):
         x=x.replace(' ','_')
         x="approach/"+x
         a.append(x)
+        img="/media/"+str(i.image)
+        a.append(img)
         data.append(a)
-    return render(request,'index.html',context={'data':data})
+    obj=events.objects.filter(date__gte=datetime.datetime.now()).order_by('date')
+    eves=[]
+    ct=0
+    for i in obj:
+        a=[]
+        a.append(i.name)
+        x=str(i.date)
+        x=x.split('-')
+        x=x[2]+'-'+x[1]+'-'+x[0]
+        a.append(x)
+        img="/media/"+str(i.image)
+        a.append(img)
+        eves.append(a)
+        ct+=1
+        if ct==2:
+            break
+
+    return render(request,'index.html',context={'data':data,'no_of_events':ct,'events':eves})
 
 def pre(request):
     return render(request,'pre.html')
